6dcc5abf8f7dfe62177c2a564c46eef89c075193
meteoben <meteoben@gmail.com>
2023-12-20 09:32:31 +0100

    ok menu et ajout chevron

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,81 +1,97 @@
-import { useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 // import { ReactComponent as Hamburger } from '../../assets/icons/hamburger.svg'
 // import { ReactComponent as Brand } from '../logo.svg'
 // import { logo as Brand } from '../logo.jpg'
-import React from 'react'
+import React from "react";
 // import logo from '../logo-png-mini2.webp'
 
-import './navbar.scss'
-import { Squash as Hamburger } from 'hamburger-react'
+import "./navbar.scss";
+import { Squash as Hamburger } from "hamburger-react";
 // import { Squeeze as Hamburger } from 'hamburger-react'
 
-
 const Navbar = () => {
-    const [showNavbar, setShowNavbar] = useState(false)
+  const [showNavbar, setShowNavbar] = useState(false);
 
-    const handleShowNavbar = () => {
-        setShowNavbar(!showNavbar)
-    }
-    const [open, setOpen] = React.useState(false);
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
+  const [open, setOpen] = React.useState(false);
 
-    const handleOpen = () => {
-        setOpen(!open);
-    };
+  const handleOpen = () => {
+    setOpen(!open);
+  };
 
+  return (
+    <header>
+      <nav className="navbar">
+        <div className="container">
+          {/* <div className="logo"> */}
+          {/* <Brand /> */}
+          {/* <img src={logo} className='logo-1' alt="logo" /> */}
+          <div className="brand">METEOBEN</div>
+          {/* </div> */}
+          <div className="menu-icon" onClick={handleShowNavbar}>
+            {/* <Hamburger /> */}
+            <span className="burger-icon">
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+          </div>
+          <div className={`nav-elements  ${showNavbar && "active"}`}>
+            <ul>
+              <li>
+                <NavLink to="/" onClick={handleShowNavbar}>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/mission" onClick={handleShowNavbar}>
+                  Mission
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/methodologie" onClick={handleShowNavbar}>
+                  Methodologie
+                </NavLink>
+              </li>
 
-    return (
-        <header>
-            <nav className="navbar">
-                <div className="container">
-                    {/* <div className="logo"> */}
-                    {/* <Brand /> */}
-                    {/* <img src={logo} className='logo-1' alt="logo" /> */}
-                    <div className="brand">METEOBEN</div>
-                    {/* </div> */}
-                    <div className="menu-icon" onClick={handleShowNavbar}>
-                        <Hamburger />
-                    </div>
-                    <div className={`nav-elements  ${showNavbar && 'active'}`}>
-                        <ul>
-                            <li>
-                                <NavLink to="/" onClick={handleShowNavbar}>Home</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/mission" onClick={handleShowNavbar}>Mission</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/methodologie" onClick={handleShowNavbar}>Methodologie</NavLink>
-                            </li>
+              {/* <div className="dropdown"> */}
+              <li className="drop" onClick={handleOpen}>
+              <div className="drop-icon">Prestations<i className="fas fa-chevron-down"></i></div>
 
-                            {/* <div className="dropdown"> */}
-                            <li className="drop" onClick={handleOpen}>Contact
-                                {open ? (
-                                    <ul className="menu">
-                                        <li className="menu-item">
-                                            <NavLink to="/contact" onClick={handleShowNavbar}>Contact</NavLink>
-                                        </li>
-                                        <li className="menu-item">
-                                            <NavLink to="/contact" onClick={handleShowNavbar}>Contact</NavLink>
-                                        </li>
-                                    </ul>
-                                ) : null}
-                                {/* {open ? <div>Is Open</div> : <div>Is Closed</div>} */}
-                            </li>
-                            <li>
-                                <NavLink to="/contact" onClick={handleShowNavbar}>Contact</NavLink>
-                            </li>
-                        </ul>
+                {open ? (
+                  <ul className="menu">
+                    <li className="menu-item">
+                      <NavLink to="/contact" onClick={handleShowNavbar}>
+                        Contact
+                      </NavLink>
+                    </li>
+                    <li className="menu-item">
+                      <NavLink to="/contact" onClick={handleShowNavbar}>
+                        Contact
+                      </NavLink>
+                    </li>
+                  </ul>
+                ) : null}
+                {/* {open ? <div>Is Open</div> : <div>Is Closed</div>} */}
+              </li>
+              <li>
+                <NavLink to="/contact" onClick={handleShowNavbar}>
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          {/* </div> */}
+        </div>
+      </nav>
+      <div className="title-container">
+        <h1>Ekklesia Web, dynamiser votre vie d'église</h1>
+      </div>
+    </header>
+  );
+};
 
-                    </div>
-                    {/* </div> */}
-                </div>
-            </nav>
-            <div className="title-container">
-                <h1>Ekklesia Web, dynamiser votre vie d'église</h1>
-            </div>
-        </header>
-    )
-}
-
-export default Navbar
+export default Navbar;
