d27e505fb0b36c0696ac24db1952150d3ddfaf92
meteoben <meteoben@gmail.com>
2024-02-24 14:58:53 +0100

    ajout du useeffect scrollTo page contact pour mobile

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ContactForm from "../../components/ContactForm/ContactForm";
-
+import { useEffect } from "react";
 
 
 export default function ContactPage() {
-
+    useEffect(() => {
+        window.scrollTo(0, 0);
+      }, []);
 return(
     <main>
 
